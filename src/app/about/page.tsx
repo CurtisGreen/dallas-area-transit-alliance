55ad313cb583849920dd5cce8291151d41c1a208
cullam3n <cullam3n@gmail.com>
2024-07-28 18:49:27 -0500

    Update page.tsx
    leading-tight

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function Index() {
       Our Mission
 	    </h1>
 	    <div>
-		    <h2 className="text-3xl mt-12 mb-4 leading-snug leading-tight">
+		    <h2 className="text-3xl mt-12 mb-4 leading-tight">
         Fighting for affordable, accessible, and reliable public transit for the continually growing Dallas area.  
 	      </h2>
       </div>
@@ -18,7 +18,7 @@ export default function Index() {
         Since forming in 2024, the Dallas Area Transit Alliance (DATA) has become a movement to shine a light on vital issues with public transit in Dallas and the surrounding cities.  DATA has mobilized to address concerns with local officials and representatives, organized with our local communities, and correspond with media to make our voices heard.  We are committed to improving public transit holistically, including advocating for better land use.
         </div>
         <div className="mb-4 font-bold">Our goals are simple but evergreen:</div>
-        <ol className="text-1xl leading-snug">
+        <ol className="text-1xl leading-tight">
           <li>1. Ensure public transit remains a viable option for everyone in the Dallas area </li>
           <li>2. Engage and collaborate with elected officials to promote transit positive agendas</li>
           <li>3. Identify problem areas with transit and provide desirable solutions</li>
