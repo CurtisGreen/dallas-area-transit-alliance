ab6ad610dd35660de2f38f2ef2be788f68ec5131
cullam3n <cullam3n@gmail.com>
2024-07-28 18:37:58 -0500

    Update page.tsx
    visually pleasing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,28 @@
 export default function Index() {
   return (
     <>
-      <h1 className="my-16 md:mb-12 text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="my-16 md:mb-6 text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
       Our Mission
 	    </h1>
 	    <div>
-		  Fighting for affordable, accessible, and reliable public transit for the continually growing Dallas area.  
-	    </div>
-	    <h1>
-        About Us
+		    <h2 className="text-3xl mt-12 mb-4 leading-snug leading-tight">
+        Fighting for affordable, accessible, and reliable public transit for the continually growing Dallas area.  
+	      </h2>
+      </div>
+	  <div>
+      <h1 className="my-8 md:mb-3 md:text-4xl font-bold leading-tight">
+      About Us
       </h1>
-      <div>
+    </div>
+      <div className="my-8 md:mb-6 text-2xl md:text-2xl leading-tight md:pr-8">
         Since forming in 2024, the Dallas Area Transit Alliance (DATA) has become a movement to shine a light on vital issues with public transit in Dallas and the surrounding cities.  DATA has mobilized to address concerns with local officials and representatives, organized with our local communities, and correspond with media to make our voices heard.  We are committed to improving public transit holistically, including advocating for better land use.
-        <div className="mb-4">Our goals are simple but evergreen:</div>
-        <ol>
+        </div>
+        <div className="mb-4 font-bold">Our goals are simple but evergreen:</div>
+        <ol className="text-1xl leading-snug">
           <li>1. Ensure public transit remains a viable option for everyone in the Dallas area </li>
           <li>2. Engage and collaborate with elected officials to promote transit positive agendas</li>
           <li>3. Identify problem areas with transit and provide desirable solutions</li>
         </ol>
-      </div>
     </>
   );
 }
