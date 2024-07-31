583404410204774532f73d8f21c9feddabc59731
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2024-07-30 21:15:31 -0500

    add external url check

--- src/app/_components/banner-body.tsx
+++ src/app/_components/banner-body.tsx
@@ -10,15 +10,20 @@ type Props = {
 
 export const BannerBody = ({ text, href }: Props) => {
   const pathname = usePathname();
+  const isExternalHref = href.startsWith("http");
 
-  if (pathname === href) {
+  if (pathname === href && !isExternalHref) {
     return null;
   } else {
     return (
       <div className="text-slate-900 dark:text-white bg-neutral-300 dark:bg-slate-950 border-b border-neutral-800 dark:border-neutral-200">
         <Container>
           <div className="py-3 text-center">
-            <a className="underline" href={href}>
+            <a
+              className="underline"
+              href={href}
+              target={isExternalHref ? "_blank" : undefined}
+            >
               {text}
             </a>
           </div>
