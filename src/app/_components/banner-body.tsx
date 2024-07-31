26b46c98a8b022396e0249aa706c366b4fe66909
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2024-07-30 22:20:58 -0500

    make banner bg SLIGHTLY lighter on light mode

--- src/app/_components/banner-body.tsx
+++ src/app/_components/banner-body.tsx
@@ -16,7 +16,7 @@ export const BannerBody = ({ text, href }: Props) => {
     return null;
   } else {
     return (
-      <div className="text-slate-900 dark:text-white bg-neutral-300 dark:bg-slate-950 border-b border-neutral-800 dark:border-neutral-200">
+      <div className="text-slate-900 dark:text-white bg-neutral-200 dark:bg-slate-950 border-b border-neutral-800 dark:border-neutral-200">
         <Container>
           <div className="py-3 text-center">
             <a
