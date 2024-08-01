3232f9e8d3b1d4404cd7684814ef715e91f64404
CurtisGreen <c.green5201@gmail.com>
2024-08-01 11:19:50 -0500

    Add all social media links to the contact page (#31)
    * Add all social media links to the contact page
    
    * Fix light mode
    
    * Move groupme to the front

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,9 @@
 import { DiscordIcon } from "@/app/_components/discord-icon";
 import { GroupMeIcon } from "@/app/_components/groupme-icon";
+import { FacebookIcon } from "@/app/_components/facebook-icon";
+import { InstagramIcon } from "@/app/_components/instagram-icon";
+import { XIcon } from "@/app/_components/x-icon";
+import { BlueskyIcon } from "@/app/_components/bluesky-icon";
 
 export default function Index() {
   return (
@@ -7,17 +11,36 @@ export default function Index() {
       <h1 className="my-16 md:mb-12 text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
         Contact Us
       </h1>
-      <div>
-        <div className="pb-4">
+      <div className="text-lg">
+        <div className="mb-2">Follow us on social media</div>
+        <div className="flex gap-4">
+          <a href="https://www.facebook.com/groups/7092451177524504">
+            <FacebookIcon className="h-8" />
+          </a>
+          <a href="https://instagram.com/ridewithdata">
+            <InstagramIcon className="h-8" />
+          </a>
+          <a href="https://x.com/ridewithdata">
+            <XIcon className="h-8" />
+          </a>
+          <a href="https://bsky.app/profile/ridewithdata.org">
+            <BlueskyIcon className="h-8" />
+          </a>
+        </div>
+
+        <div className="mb-2 mt-10">Or join the conversation!</div>
+        <div className="mb-4 flex gap-6">
+          <a href="https://groupme.com/join_group/92468276/JsoCAH57">
+            <GroupMeIcon className="h-9 fill-slate-700 dark:fill-white hover:fill-blue-700 dark:hover:fill-blue-700" />
+          </a>
           <a href="https://discord.gg/ZKwDE2s4qx">
             <DiscordIcon className="h-8 fill-slate-700 dark:fill-white hover:fill-blue-700 dark:hover:fill-blue-700" />
           </a>
         </div>
 
-        <div>
-          <a href="https://groupme.com/join_group/92468276/JsoCAH57">
-            <GroupMeIcon className="h-9 fill-slate-700 dark:fill-white hover:fill-blue-700 dark:hover:fill-blue-700" />
-          </a>
+        <div className="mb-2 mt-10">Press Contact</div>
+        <div className="font-semibold dark:text-white dark:font-normal">
+          info@ridewithdata.org
         </div>
       </div>
     </>
