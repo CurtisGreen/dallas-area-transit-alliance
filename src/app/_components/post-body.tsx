3068b4eba023042301bed7ea4b765e7db81bde01
unknown <c.green5201@gmail.com>
2024-08-01 00:52:42 -0500

    Widen post width and shrink post titles

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-3xl mx-auto">
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
