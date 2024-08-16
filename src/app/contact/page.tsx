33de6b697564e849b0c831cb38862ca4bfe84261
Qui Le <26169690+LEQUI000@users.noreply.github.com>
2024-08-15 22:53:27 -0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -40,7 +40,7 @@ export default function Index() {
 
         <div className="mb-2 mt-10">Press Contact</div>
         <div className="font-semibold dark:text-white dark:font-normal">
-        <a href="mailto:info@ridewithdata.org">info@ridewithdata.org</a>
+         <a href="mailto:info@ridewithdata.org">info@ridewithdata.org</a>
         </div>
         
         <div className="mb-2 mt-10">Contact Us</div>
