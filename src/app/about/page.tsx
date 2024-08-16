e3df21ff3b70f8ed1b4b1fbcece22d04e79748af
Qui Le <26169690+LEQUI000@users.noreply.github.com>
2024-08-15 22:21:02 -0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -63,16 +63,6 @@ export default function Index() {
         </li>
         </ol>
         </div>
-      <div>
-        <iframe 
-          src="https://docs.google.com/forms/d/e/1FAIpQLSeE-SrnsbjniO7I0BkdHourNdQYO_MKaYhKa6iQjN63_eubBg/viewform?embedded=true" 
-          width="640" 
-          height="2875" 
-          frameBorder="0" 
-          marginHeight="0" 
-          marginWidth="0">
-        </iframe>
-      </div>
     </div>
   );
 }
