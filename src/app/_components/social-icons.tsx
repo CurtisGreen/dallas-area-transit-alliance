37dc016ff6e1a8602c9104e2ed0c01d63861a250
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2024-08-31 20:55:56 -0500

    Footer revamp (#49)
    * redo footer
    
    * put icons in folder
    
    * more icons!!
    
    * sneak this into the readme

---
+++ src/app/_components/social-icons.tsx
@@ -0,0 +1,41 @@
+import { BlueskyIcon } from "@/app/_components/icons/bluesky-icon";
+import { FacebookIcon } from "@/app/_components/icons/facebook-icon";
+import { GitHubIcon } from "@/app/_components/icons/github-icon";
+import { InstagramIcon } from "@/app/_components/icons/instagram-icon";
+import { LinkedInIcon } from "@/app/_components/icons/linkedin-icon";
+import { RedditIcon } from "@/app/_components/icons/reddit-icon";
+import { XIcon } from "@/app/_components/icons/x-icon";
+
+type Props = {
+  showGitHub?: boolean;
+};
+
+export function SocialIcons({ showGitHub }: Props) {
+  return (
+    <div className="flex flex-wrap gap-4">
+      <a href="https://www.facebook.com/groups/7092451177524504">
+        <FacebookIcon className="h-8" />
+      </a>
+      <a href="https://instagram.com/ridewithdata">
+        <InstagramIcon className="h-8" />
+      </a>
+      <a href="https://x.com/ridewithdata">
+        <XIcon className="h-8" />
+      </a>
+      <a href="https://bsky.app/profile/ridewithdata.org">
+        <BlueskyIcon className="h-8" />
+      </a>
+      <a href="https://www.reddit.com/user/ridewithdata/">
+        <RedditIcon className="h-8" />
+      </a>
+      <a href="https://www.linkedin.com/company/ridewithdata/">
+        <LinkedInIcon className="h-8" />
+      </a>
+      {showGitHub && (
+        <a href="https://github.com/CurtisGreen/dallas-area-transit-alliance">
+          <GitHubIcon className="h-8" />
+        </a>
+      )}
+    </div>
+  );
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,6 @@
-import { DiscordIcon } from "@/app/_components/discord-icon";
-import { GroupMeIcon } from "@/app/_components/groupme-icon";
-import { FacebookIcon } from "@/app/_components/facebook-icon";
-import { InstagramIcon } from "@/app/_components/instagram-icon";
-import { XIcon } from "@/app/_components/x-icon";
-import { BlueskyIcon } from "@/app/_components/bluesky-icon";
+import { DiscordIcon } from "@/app/_components/icons/discord-icon";
+import { GroupMeIcon } from "@/app/_components/icons/groupme-icon";
+import { SocialIcons } from "@/app/_components/social-icons";
 
 export default function Index() {
   return (
@@ -13,22 +10,9 @@ export default function Index() {
       </h1>
       <div className="text-lg">
         <div className="mb-2">Follow us on social media</div>
-        <div className="flex gap-4">
-          <a href="https://www.facebook.com/groups/7092451177524504">
-            <FacebookIcon className="h-8" />
-          </a>
-          <a href="https://instagram.com/ridewithdata">
-            <InstagramIcon className="h-8" />
-          </a>
-          <a href="https://x.com/ridewithdata">
-            <XIcon className="h-8" />
-          </a>
-          <a href="https://bsky.app/profile/ridewithdata.org">
-            <BlueskyIcon className="h-8" />
-          </a>
-        </div>
+        <SocialIcons />
         <div className="mb-2 mt-10">Or join the conversation!</div>
-        <div className="mb-4 flex gap-6">
+        <div className="mb-4 flex flex-wrap gap-6">
           <a href="https://groupme.com/join_group/92468276/JsoCAH57">
             <GroupMeIcon className="h-9 fill-slate-700 dark:fill-white hover:fill-blue-700 dark:hover:fill-blue-700" />
           </a>
