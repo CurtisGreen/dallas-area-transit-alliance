fcb35d27b2880f9b28672b8494b9c2e024fd8c44
CurtisGreen <c.green5201@gmail.com>
2024-10-09 22:58:57 -0500

    October meeting + other updates (#56)
    * Add new event, shrink form height, and remove wwd banner
    
    * Add agenda links and update styling

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -5,6 +5,18 @@ export const revalidate = 3600;
 
 const scheduleData = [
   {
+    title: "DATA October General Meeting",
+    date: new Date("2024-10-19 00:00-05:00"),
+    points: [
+      "Saturday, October 19, 2024 from 2:00 - 4:00PM",
+      "J. Erik Jonsson Central Library at 1515 Young Street, Dallas",
+      "Hamon Training Room, 5th floor",
+      <i>
+        Disclosure: This event is not sponsored by the Dallas Public Library
+      </i>,
+    ],
+  },
+  {
     title: "Week Without Driving",
     date: new Date("2024-10-07 00:00-05:00"),
     points: [
@@ -55,6 +67,24 @@ const scheduleData = [
           <div>Sept 21 2:50PM</div>
         </div>
       </>,
+      <Link
+        href="https://docs.google.com/document/d/1M7-IS2JjXPPeE9pMLfwlhCjHqUlHczwBEYSWs1e316k"
+        className="underline"
+      >
+        Meeting agenda
+      </Link>,
+      <Link
+        href="https://drive.google.com/file/d/1dBjeae9kadSivVdb90S0wb2eAitrLRNv"
+        className="underline"
+      >
+        Meeting recording
+      </Link>,
+      <Link
+        href="https://docs.google.com/document/d/1A6VkltTQvLb_tDSgPfe9DDb7Ee4JmDatXhn4AdVgb2M"
+        className="underline"
+      >
+        Meeting transcript
+      </Link>,
     ],
   },
   {
@@ -123,7 +153,7 @@ const scheduleData = [
 
 export default function Index() {
   return (
-    <div className="leading-snug mb-10">
+    <div className="leading-snug mb-20">
       <div className="my-12 md:my-16 md:mb-12 text-5xl md:text-7xl font-bold tracking-tighter">
         Calendar
       </div>
@@ -136,7 +166,7 @@ export default function Index() {
         .sort((a, b) => a.date.valueOf() - b.date.valueOf())
         .map(({ title, date, points }) => (
           <div key={date.valueOf()}>
-            <div className="text-lg mt-8 font-medium">{title}</div>
+            <div className="text-lg mt-8 font-semibold">{title}</div>
             <ul>
               {points.map((point) => (
                 <li key={point.toString()}>{point}</li>
@@ -153,7 +183,7 @@ export default function Index() {
         .sort((a, b) => b.date.valueOf() - a.date.valueOf())
         .map(({ title, date, points }) => (
           <div key={date.valueOf()}>
-            <div className="text-lg mt-5">{title}</div>
+            <div className="text-lg mt-8 font-semibold">{title}</div>
             <ul>
               {points.map((point) => (
                 <li key={point.toString()}>{point}</li>
