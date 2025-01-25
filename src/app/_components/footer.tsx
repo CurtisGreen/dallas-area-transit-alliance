d172944994cf1ec3f54a6f2959dbf9cb2271f1db
Qui Le <26169690+LEQUI000@users.noreply.github.com>
2025-01-25 19:42:19 +0000

    fix year

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -28,7 +28,7 @@ export function Footer() {
             <Logo className="w-64" />
           </div>
           <div className="flex flex-col justify-center items-center lg:items-start lg:pl-4 lg:w-1/2 xl:2-1/3">
-            <div className="mb-2">&copy; 2024 Dallas Area Transit Alliance</div>
+            <div className="mb-2">&copy; 2025 Dallas Area Transit Alliance</div>
             <div className="mb-4 text-center lg:text-left">
               Dallas Area Transit Alliance (DATA) is a rider-run advocacy group
               that is not affiliated with Dallas Area Rapid Transit (DART) or
