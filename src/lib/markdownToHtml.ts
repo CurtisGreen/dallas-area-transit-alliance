2e3b032bc0d557f27d5adb1ca548d4686ce1e456
CurtisGreen <c.green5201@gmail.com>
2025-02-27 20:27:44 -0600

    Strikethrough in markdown, fix link overflow on mobile (#82)

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import { remark } from "remark";
 import html from "remark-html";
+import remarkGfm from "remark-gfm";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(html).use(remarkGfm).process(markdown);
   return result.toString();
 }
