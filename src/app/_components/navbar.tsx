bed36203a41d6f4bd029217a623aecb015012958
Qui Le <26169690+LEQUI000@users.noreply.github.com>
2025-03-05 00:33:57 -0600

    Update navbar.tsx

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -63,7 +63,7 @@ export const Navbar = () => {
                   href="https://actionnetwork.org/letters/save-dart-from-funding-cuts?source=website_navbar&"
                   target="_blank"
                 >
-                  Contact your State Reps
+                  Say NO! to HB3187 and SB1557
                 </NavbarLink>
               </div>
               <div>
