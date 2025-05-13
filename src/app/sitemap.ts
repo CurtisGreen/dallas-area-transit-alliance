b43d342a4ca9a8cb31069e67dcbf6cbeca9c7723
unknown <c.green5201@gmail.com>
2025-05-13 16:45:02 -0500

    Update sitemap with privacy policy

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -38,6 +38,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.5,
     },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
     ...postSitemaps,
   ];
 }
