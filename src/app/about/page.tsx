040cd681d1a26749d69abd64b8e37c5677c53137
Qui Le <26169690+LEQUI000@users.noreply.github.com>
2025-05-19 10:26:23 -0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,37 +15,36 @@ export default function Index() {
       </div>
       <ul className="mb-16 md:mb-12 text-lg">
         <li className="mb-1">
-          Connor Hulla, President
+          Connor Hulla, President, <a href="mailto:president@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
-          Tyler Wright, Vice President
+          Tyler Wright, Vice President, <a href="mailto:vp@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
-          Alexander Dunn, Secretary
+          Alexander Dunn, Secretarym <a href="mailto:secretary@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
-          Patric Morgan, Parliamentarian
+          Patric Morgan, Parliamentarian <a href="mailto:rules@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
           To be appointed, Treasurer
         </li>
         <li className="mb-1">
-          Qui Le, Systems Administrator
+          Qui Le, Systems Administrator <a href="mailto:sysadmin@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
-          David Yaqubuin, External Affairs Chair
+          David Yaqubuin, External Affairs Chair <a href="mailto:external-affairs@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
-          Rohan Kodibagkar, Community Outreach Chair
+          Rohan Kodibagkar, Community Outreach Chair <a href="mailto:community-outreach@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
-          Keun Lee, Policy Research Chair
+          Keun Lee, Policy Research Chair <a href="mailto:policy-research@ridewithdata.org"Email> </a>
         </li>
         <li className="mb-1">
-          Qui Le, Event Planning Chair
+          Qui Le, Event Planning Chair <a href="mailto:events@ridewithdata.org"Email> </a>
         </li>
       </ul>
-      <p>Want to help us out? <a href="mailto:info@ridewithdata.org">Email us</a> if you are interested in an Executive Committee Position. </p>
       <div className="text-2xl md:text-3xl mt-8 md:mt-12 mb-4 font-bold">
         History
       </div>
