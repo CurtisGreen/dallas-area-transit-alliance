2aeb93751f2543dbece53cc8426782b304d7fdc3
unknown <c.green5201@gmail.com>
2025-08-03 22:33:39 -0500

    dont specify cache

--- src/lib/substack-api.ts
+++ src/lib/substack-api.ts
@@ -32,7 +32,7 @@ type Article = {
 
 export const getSubstackArticles = async (name: string, count: number) => {
   const url = `https://substackapi.com/api/feeds/${name}.substack.com?limit=${count}&sort=new`;
-  const data = await fetch(url, { cache: "no-store" });
+  const data = await fetch(url);
   const articles: Article[] = await data.json();
   return articles;
 };
