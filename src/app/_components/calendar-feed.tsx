d6e9ca62b2c5d230cc60814c6eb2320034e19654
Qui Le <26169690+LEQUI000@users.noreply.github.com>
2026-07-26 20:10:46 -0600

    Fix wording

--- src/app/_components/calendar-feed.tsx
+++ src/app/_components/calendar-feed.tsx
@@ -15,6 +15,9 @@ export const CalendarFeed = async () => {
     <div>
       <div className="text-3xl md:text-4xl mt-8 md:mt-12 mb-4 font-bold">
         Upcoming events (All times in Central Time)
+        <div className="text-2xl md:text-3xl mt-8 md:mt-12 mb-4 italic">
+        All times in Central Time
+        </div>
       </div>
       <CalendarEvents
         events={events.slice(0, 10)}
@@ -31,8 +34,8 @@ export const CalendarFeed = async () => {
         >
           See more on our calendar
         </Link>
+        </div>
       </div>
-    </div>
   );
 };
 
